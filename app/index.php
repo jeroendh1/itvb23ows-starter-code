21ddb0d9689bb3fa3bb522b4d5076a6349b3498f
Jeroendh <jer.de.haan@st.hanze.nl>
2024-01-14 20:00:49 +0100

    fix (overal) Code refactored, added class game and databasehandler

--- app/index.php
+++ app/index.php
@@ -1,194 +1,149 @@
 <?php
-    session_start();
+include_once 'game.php';
+include_once 'databaseHandler.php';
 
-    include_once 'util.php';
+$db = new mysqli('db', 'root', '', 'hive');
+$dbHandler = new DbHandler($db);
+$game = new Game($dbHandler);
 
-    if (!isset($_SESSION['board'])) {
-        header('Location: restart.php');
-        exit(0);
+// Get possible positions
+$to = $game->getPossiblePositions();
+
+// Process form submissions
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['play'])) {
+        $game->play($_POST['piece'], $_POST['to']);
+    } elseif (isset($_POST['from']) && isset($_POST['to'])) {
+        $game->move($_POST['from'], $_POST['to']);
+    } elseif (isset($_POST['pass'])) {
+        $game->pass();
+    } elseif (isset($_POST['restart'])) {
+        $game->restart();
+        header('index.php');
+    } elseif (isset($_POST['undo'])) {
+        $game->undo();
     }
-    $board = $_SESSION['board'];
-    $player = $_SESSION['player'];
-    $hand = $_SESSION['hand'];
-
-    $to = [];
-    foreach ($GLOBALS['OFFSETS'] as $pq) {
-        foreach (array_keys($board) as $pos) {
-            $pq2 = explode(',', $pos);
-            
-            $possible_pos = ($pq[0] + $pq2[0]).','.($pq[1] + $pq2[1]);
-
-            if (in_array($possible_pos, array_keys($board)) or !hasNeighBour($possible_pos, $board) or (array_sum($hand) < 11 && !neighboursAreSameColor($player, $possible_pos, $board))){   
-                echo ' in',  $possible_pos ;
-                continue;
-            }
-
-            $to[] = ($pq[0] + $pq2[0]).','.($pq[1] + $pq2[1]);
-        }
-    }
-    $to = array_unique($to);
-    if (!count($to)) $to[] = '0,0';
+}
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Hive</title>
-        <style>
-            div.board {
-                width: 60%;
-                height: 100%;
-                min-height: 500px;
-                float: left;
-                overflow: scroll;
-                position: relative;
-            }
-
-            div.board div.tile {
-                position: absolute;
-            }
-
-            div.tile {
-                display: inline-block;
-                width: 4em;
-                height: 4em;
-                border: 1px solid black;
-                box-sizing: border-box;
-                font-size: 50%;
-                padding: 2px;
-            }
-
-            div.tile span {
-                display: block;
-                width: 100%;
-                text-align: center;
-                font-size: 200%;
-            }
-
-            div.player0 {
-                color: black;
-                background: white;
-            }
-
-            div.player1 {
-                color: white;
-                background: black
-            }
-
-            div.stacked {
-                border-width: 3px;
-                border-color: red;
-                padding: 0;
-            }
-        </style>
+        <link rel="stylesheet" href="/css/main.css">
     </head>
     <body>
-        <div class="board">
-            <?php
-                $min_p = 1000;
-                $min_q = 1000;
-                foreach ($board as $pos => $tile) {
-                    $pq = explode(',', $pos);
-                    if ($pq[0] < $min_p) $min_p = $pq[0];
-                    if ($pq[1] < $min_q) $min_q = $pq[1];
-                }
-                foreach (array_filter($board) as $pos => $tile) {
-                    $pq = explode(',', $pos);
-                    $pq[0];
-                    $pq[1];
-                    $h = count($tile);
-                    echo '<div class="tile player';
-                    echo $tile[$h-1][0];
-                    if ($h > 1) echo ' stacked';
-                    echo '" style="left: ';
-                    echo ($pq[0] - $min_p) * 4 + ($pq[1] - $min_q) * 2;
-                    echo 'em; top: ';
-                    echo ($pq[1] - $min_q) * 4;
-                    echo "em;\">($pq[0],$pq[1])<span>";
-                    echo $tile[$h-1][1];
-                    echo '</span></div>';
-                }
-            ?>
-        </div>
-        <div class="hand">
-            White:
-            <?php
-                foreach ($hand[0] as $tile => $ct) {
-                    for ($i = 0; $i < $ct; $i++) {
-                        echo '<div class="tile player0"><span>'.$tile."</span></div> ";
-                    }
-                }
-            ?>
-        </div>
-        <div class="hand">
-            Black:
-            <?php
-            foreach ($hand[1] as $tile => $ct) {
+    <div class="board">
+        <?php
+            // PHP code to render the game board
+            $min_p = 1000;
+            $min_q = 1000;
+            foreach ($game->board as $pos => $tile) {
+                $pq = explode(',', $pos);
+                if ($pq[0] < $min_p) $min_p = $pq[0];
+                if ($pq[1] < $min_q) $min_q = $pq[1];
+            }
+            foreach (array_filter($game->board) as $pos => $tile) {
+                $pq = explode(',', $pos);
+                $pq[0];
+                $pq[1];
+                $h = count($tile);
+                echo '<div class="tile player';
+                echo $tile[$h-1][0];
+                if ($h > 1) echo ' stacked';
+                echo '" style="left: ';
+                echo ($pq[0] - $min_p) * 4 + ($pq[1] - $min_q) * 2;
+                echo 'em; top: ';
+                echo ($pq[1] - $min_q) * 4;
+                echo "em;\">($pq[0],$pq[1])<span>";
+                echo $tile[$h-1][1];
+                echo '</span></div>';
+            }
+        ?>
+    </div>
+    <div class="hand">
+        White:
+        <?php
+            // PHP code to render White player's hand
+            foreach ($game->hand[0] as $tile => $ct) {
                 for ($i = 0; $i < $ct; $i++) {
-                    echo '<div class="tile player1"><span>'.$tile."</span></div> ";
+                    echo '<div class="tile player0"><span>'.$tile."</span></div> ";
                 }
             }
-            ?>
-        </div>
-        <div class="turn">
-            Turn: <?php print_r( array_keys($board));if ($player == 0) echo "White"; else echo "Black"; ?>
-        </div>
-        <form method="post" action="play.php">
-            <select name="piece">
-                <?php
-                    foreach ($hand[$player] as $tile => $ct) {
-                        if ($ct){
-                            echo "<option value=\"$tile\">$tile</option>";
-                        }
-                    }
-                ?>
-            </select>
-            <select name="to">
-                <?php
-                    foreach ($to as $pos) {
-                        echo "<option value=\"$pos\">$pos</option>";
-                    }
-                ?>
-            </select>
-            <input type="submit" value="Play">
-        </form>
-        <form method="post" action="move.php">
-            <select name="from">
-                <?php
-                    foreach (array_keys($board) as $pos) {
-                        echo "<option value=\"$pos\">$pos</option>";
-                    }
-                ?>
-            </select>
-            <select name="to">
-                <?php
-                    foreach ($to as $pos) {
-                        echo "<option value=\"$pos\">$pos</option>";
-                    }
-                ?>
-            </select>
-            <input type="submit" value="Move">
-        </form>
-        <form method="post" action="pass.php">
-            <input type="submit" value="Pass">
-        </form>
-        <form method="post" action="restart.php">
-            <input type="submit" value="Restart">
-        </form>
-        <strong><?php if (isset($_SESSION['error'])) echo($_SESSION['error']); unset($_SESSION['error']); ?></strong>
-        <ol>
+        ?>
+    </div>
+    <div class="hand">
+        Black:
+        <?php
+        // PHP code to render Black player's hand
+        foreach ($game->hand[1] as $tile => $ct) {
+            for ($i = 0; $i < $ct; $i++) {
+                echo '<div class="tile player1"><span>'.$tile."</span></div> ";
+            }
+        }
+        ?>
+    </div>
+    <div class="turn">
+        Turn: <?php echo $game->getCurrentPlayerColor(); ?>
+    </div>
+    <form method="post" action="index.php">
+        <select name="piece">
             <?php
-                $db = include 'database.php';
-                $stmt = $db->prepare('SELECT * FROM moves WHERE game_id = '.$_SESSION['game_id']);
-                $stmt->execute();
-                $result = $stmt->get_result();
-                while ($row = $result->fetch_array()) {
-                    echo '<li>'.$row[2].' '.$row[3].' '.$row[4].'</li>';
+                // PHP code to render the player's available pieces
+                foreach ($game->hand[$game->player] as $tile => $ct) {
+                    if ($ct){
+                        echo "<option value=\"$tile\">$tile</option>";
+                    }
                 }
             ?>
-        </ol>
-        <form method="post" action="undo.php">
-            <input type="submit" value="Undo">
-        </form>
+        </select>
+        <select name="to">
+            <?php
+                // PHP code to render the available positions
+                foreach ($game->getPossiblePositions() as $pos) {
+                    echo "<option value=\"$pos\">$pos</option>";
+                }
+            ?>
+        </select>
+        <input type="submit" name='play' value="Play">
+    </form>
+    <form method="post" action="index.php">
+        <select name="from">
+            <?php
+                // PHP code to render the player's pieces
+                foreach (array_keys($game->board) as $pos) {
+                    echo "<option value=\"$pos\">$pos</option>";
+                }
+            ?>
+        </select>
+        <select name="to">
+            <?php
+                // PHP code to render the available positions for moving
+                foreach ($game->getPossiblePositions() as $pos) {
+                    echo "<option value=\"$pos\">$pos</option>";
+                }
+            ?>
+        </select>
+        <input type="submit" value="Move">
+    </form>
+    <form method="post" action="index.php">
+        <input type="submit" name="pass" value="Pass">
+    </form>
+    <form method="post" action="index.php">
+        <input type="submit" name="restart" value="Restart">
+    </form>
+    <strong><?php if (isset($_SESSION['error'])) echo($_SESSION['error']); unset($_SESSION['error']); ?></strong>
+    <ol>
+        <?php
+            // PHP code to render the list of moves
+            $result = $dbHandler->getGameMoves($_SESSION['game_id']);
+            while ($row = $result->fetch_array()) {
+                echo '<li>'.$row[2].' '.$row[3].' '.$row[4].'</li>';
+            }
+        ?>
+    </ol>
+    <form method="post" action="index.php">
+        <input type="submit" name='undo'value="Undo">
+    </form>
     </body>
 </html>
 
